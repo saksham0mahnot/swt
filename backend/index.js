1fdb0a017e689cb83b8c59181ba9496ccf66753e
saksham0mahnot <saksham.mahnot@gmail.com>
2025-11-19 13:46:41 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-// swt-services/index.js
+// backend/index.js
 const serverless = require('serverless-http');
-const app = require('./src/app'); // ensure this exports the express `app` (see below)
+const app = require('./src/app'); // ensure src/app exports an Express app
 
 module.exports = serverless(app);
